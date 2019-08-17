dd2e62ab32ea58b956865c5c81efd103f61a9608
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-08-17 16:38:54 +0300

    Services list on main page

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\BrandRepository;
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +12,16 @@ class PageController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(BrandRepository $brand_repository)
+    public function index(BrandRepository $brand_repository, ServiceRepository $service_repository)
     {
-        $brands = $brand_repository->findAll();
+        $brands     = $brand_repository->findAll();
+        $breakdowns = $service_repository->findBy(['is_service' => false]);
+        $services   = $service_repository->findBy(['is_service' => true]);
+        
         return $this->render('page/index.html.twig', [
-            'brands' => $brands,
+            'brands'     => $brands,
+            'breakdowns' => $breakdowns,
+            'services'   => $services,
         ]);
     }
 }
