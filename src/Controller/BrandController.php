179f6565bf81368df8aef8b41febe0c6820a7408
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-08-17 18:28:06 +0300

    Brand, Model and Service templates

---
+++ src/Controller/BrandController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BrandRepository;
+use App\Repository\ModelRepository;
+use App\Repository\ServiceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BrandController extends AbstractController
+{
+    /**
+     * @Route("/remont-kofemashin-{brand}/", name="brand")
+     */
+    public function index($brand, BrandRepository $brand_repository, ServiceRepository $service_repository)
+    {
+        if ( ! $brand_entity = $brand_repository->findOneBy(['alias' => $brand])) {
+            throw new NotFoundHttpException();
+        }
+        $breakdowns = $service_repository->findBy(['is_service' => false]);
+        $services   = $service_repository->findBy(['is_service' => true]);
+        
+        return $this->render('brand/index.html.twig', [
+            'brand' => $brand_entity,
+            'breakdowns' => $breakdowns,
+            'services' => $services,
+        ]);
+    }
+    
+    /**
+     * @Route("/remont-kofemashin-{brand}/{model_or_service}/", name="model_or_service")
+     */
+    public function modelOrService(
+        $brand,
+        $model_or_service,
+        BrandRepository $brand_repository,
+        ModelRepository $model_repository,
+        ServiceRepository $service_repository
+    ) {
+        if ( ! $brand_entity = $brand_repository->findOneBy(['alias' => $brand])) {
+            throw new NotFoundHttpException();
+        }
+        $breakdowns = $service_repository->findBy(['is_service' => false]);
+        $services   = $service_repository->findBy(['is_service' => true]);
+        if ($model_entity = $model_repository->findOneBy(['alias' => $model_or_service])) {
+            return $this->render('brand/model.html.twig', [
+                'brand' => $brand_entity,
+                'model' => $model_entity,
+                'breakdowns' => $breakdowns,
+                'services' => $services,
+            ]);
+        } elseif ($service_entity = $service_repository->findOneBy(['alias' => $model_or_service])) {
+            return $this->render('brand/service.html.twig', [
+                'brand' => $brand_entity,
+                'service' => $service_entity,
+                'breakdowns' => $breakdowns,
+                'services' => $services,
+            ]);
+        } else {
+            throw new NotFoundHttpException();
+        }
+        
+        
+    }
+    
+    /**
+     * @Route("/remont-kofemashin-{brand}/{model}/{service}/", name="service")
+     */
+    /*public function service($service)
+    {
+        dd($service);
+        return $this->render('brand/index.html.twig', [
+            'controller_name' => 'BrandController',
+        ]);
+    }*/
+}
